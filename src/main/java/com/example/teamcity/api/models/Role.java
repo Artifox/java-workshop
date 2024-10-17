ffef74136feca992c48ff46673ab332e73384c96
a.koptsiukh <a.koptsiukh@sdgroup.ai>
2024-10-17 12:14:06 +0400

    Add tests for roles

--- src/main/java/com/example/teamcity/api/models/Role.java
+++ src/main/java/com/example/teamcity/api/models/Role.java
@@ -1,5 +1,7 @@
 package com.example.teamcity.api.models;
 
+import com.example.teamcity.api.annotations.Parameterizable;
+import com.example.teamcity.api.enums.UserRoles;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
@@ -13,8 +15,10 @@ import lombok.*;
 public class Role extends BaseModel {
 
     @Builder.Default
-    private String roleId = "SYSTEM_ADMIN";
+    @Parameterizable
+    private UserRoles roleId = UserRoles.SYSTEM_ADMIN;
     @Builder.Default
+    @Parameterizable
     private String scope = "g";
 
 }
--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -1,8 +1,7 @@
 package com.example.teamcity.api;
 
-import com.example.teamcity.api.models.BuildType;
-import com.example.teamcity.api.models.Project;
-import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.enums.UserRoles;
+import com.example.teamcity.api.models.*;
 import com.example.teamcity.api.requests.CheckedRequests;
 import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
@@ -13,12 +12,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static com.example.teamcity.api.enums.Endpoint.*;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 import static com.example.teamcity.common.SuiteTags.REGRESSION;
-import static io.qameta.allure.Allure.step;
 
 @Tag(REGRESSION)
 public class BuildTypeTest extends BaseApiTest {
@@ -68,28 +67,39 @@ public class BuildTypeTest extends BaseApiTest {
     @Tags({@Tag("Positive"), @Tag("Roles")})
     @DisplayName("Project admin should be able to create build type for their project")
     void projectAdminCreatesBuildTypeTest() {
-        step("Create user");
-        step("Create project");
-        step("Grant user PROJECT_ADMIN role in project");
+        var project = superUserCheckedRequests.<Project>getRequest(PROJECTS).create(testData.getProject());
 
-        step("Create buildType for project by user (PROJECT_ADMIN)");
-        step("Check buildType was created successfully");
+        //дай, пожалуйста, ссылку на документацию, где показано, что нужно именно так нужно указывать скоуп, чтобы заасайнить юзера на проект
+        //я взял из чата, сам не нашел
+        testData.getUser().setRoles(generate(Roles.class, UserRoles.PROJECT_ADMIN, "p:" + project.getId()));
+        var user = superUserCheckedRequests.<User>getRequest(USERS).create(testData.getUser());
+        var userCheckRequests = new CheckedRequests(Specifications.authSpec(user));
+        var createdBuildType = userCheckRequests.<BuildType>getRequest(BUILD_TYPES).create(testData.getBuildType());
+        softy.assertThat(testData.getBuildType().getName())
+                .as("Check buildType name. Expected: %s, but Actual: %s", testData.getBuildType().getName(), createdBuildType.getName())
+                .isEqualTo(createdBuildType.getName());
     }
 
     @Test
     @Tags({@Tag("Negative"), @Tag("Roles")})
     @DisplayName("Project admin should not be able to create build type for not their project")
     void projectAdminCreatesBuildTypeForAnotherUserProjectTest() {
-        step("Create user1");
-        step("Create project1");
-        step("Grant user1 PROJECT_ADMIN role in project1");
+        var project1 = superUserCheckedRequests.<Project>getRequest(PROJECTS).create(generate(Project.class));
+        var project2 = superUserCheckedRequests.<Project>getRequest(PROJECTS).create(generate(Project.class));
 
-        step("Create user2");
-        step("Create project2");
-        step("Grant user2 PROJECT_ADMIN role in project2");
+        var user1 = generate(User.class);
+        user1.setRoles(generate(Roles.class, UserRoles.PROJECT_ADMIN, "p:" + project1.getId()));
+        var user2 = generate(User.class);
+        user2.setRoles(generate(Roles.class, UserRoles.PROJECT_ADMIN, "p:" + project2.getId()));
+        superUserCheckedRequests.<User>getRequest(USERS).create(user1);
+        superUserCheckedRequests.<User>getRequest(USERS).create(user2);
 
-        step("Create buildType for project1 by user2");
-        step("Check buildType was not created  with forbidden codeø");
+        testData.getBuildType().setProject(project1);
+        new UncheckedBase(Specifications.authSpec(user2), BUILD_TYPES)
+                .create(testData.getBuildType())
+                .then().assertThat()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .body(Matchers.containsString("Access denied. Check the user has enough permissions to perform the operation."));
+
     }
-
 }
